43316794183d8d18926333a2564d273ffc770dca
Frank Uzoka <Frankuzoka@hotmail.com>
2020-07-27 23:29:31 -0400

    Working on creating component

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import HelloReact from "./components/HelloReact";
+import HelloDiv from "./components/HelloDiv";
 
-const App = () => <HelloReact />;
+const App = () => <HelloDiv />;
 
 export default App;
---
+++ reactpractice/src/components/HelloDiv.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HelloDiv = () => (
+  <div>
+    <h1>Hello my name is Frank Uzoka.</h1>
+    <h2>Here are things I like</h2>
+    <li>I like to code</li>
+    <li>
+      I like to learn new things and revisit things I've learned in the past.
+    </li>
+    <li>I like to challenge possible.</li>
+  </div>
+);
+
+export default HelloDiv;
