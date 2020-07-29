8411d603577271faf658ce3566ce4f15bd54bf4b
Frank Uzoka <Frankuzoka@hotmail.com>
2020-07-28 23:55:42 -0400

    Small Change

--- reactpractice/src/components/HelloBootstrap.js
+++ reactpractice/src/components/HelloBootstrap.js
@@ -51,10 +51,7 @@ const HelloBootstrap = () => (
         This is a simple hero unit, a simple jumbotron-style component for
         calling extra attention to featured content or information.
       </p>
-      <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
-      </p>
+      <p>Working on this. Small change.</p>
       <p class="lead">
         <a class="btn btn-primary btn-lg" href="#" role="button">
           Learn more
